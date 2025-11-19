bc25bd545ad5cc7e8bcf57477a78a48e8b482867
davidandelictomasic <david.andelic.tomasic@gmail.com>
2025-11-19 10:55:37 +0100

    Add starting and passenger menu

--- Internship-3-OOP/Program.cs
+++ Internship-3-OOP/Program.cs
@@ -4,7 +4,86 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ShowStartingMenu();
         }
+
+        static void ShowStartingMenu() {
+            while (true) {
+                Console.Clear();
+                Console.WriteLine("GLAVNI IZBORNIK\n1 - Putnici\n2 - Letovi\n3 - Avioni\n4 - Posada\n5 - Izlazak iz programa");
+                Console.Write("Odabir: ");
+
+                if (int.TryParse(Console.ReadLine(), out int userChoice))
+                {
+                    switch (userChoice)
+                    {
+                        case 1:
+                            ShowPassengerMenu();
+                            break;
+                        case 2:
+                            
+                            break;
+                        case 3:
+                            
+                            break;
+                        case 4:
+
+                            break;
+                        case 5:
+                            Console.WriteLine("Izlazak iz aplikacije...");
+                            break;
+                        default:
+                            Console.WriteLine("Pogrešan unos, pokušajte ponovo.");
+                            Console.ReadKey();
+                            continue;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Pogrešan unos, pokušajte ponovo.");
+                    Console.ReadKey();
+                    continue;
+                }
+
+            }
+        
+        
+        }
+        static void ShowPassengerMenu() {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("PUTNICI IZBORNIK\n1 - Registracija\n2 - Prijava\n3 - Povratak na prethodni izbornik");
+                Console.Write("Odabir: ");
+
+                if (int.TryParse(Console.ReadLine(), out int userChoice))
+                {
+                    switch (userChoice)
+                    {
+                        case 1:
+                            
+                            break;
+                        case 2:
+
+                            break;
+                        case 3:
+
+
+                            return;
+                        default:
+                            Console.WriteLine("Pogrešan unos, pokušajte ponovo.");
+                            Console.ReadKey();
+                            continue;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Pogrešan unos, pokušajte ponovo.");
+                    Console.ReadKey();
+                    continue;
+                }
+            }
+        }
+        
     }
 }
